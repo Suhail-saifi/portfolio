d8dfcc800a7b9b088313650304cb2fb50dedd5c5
Suhail Saifi <saifisuhail441@gmail.com>
2024-04-20 00:21:24 +0530

    first changes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'navyblue': '#010020',
+        'darkerblue': '#0e0e1a'
+      },
+      fontFamily:{
+        "Poppins": ["Poppins"]
+      }
+    },
+  },
+  plugins: [],
+}
+
